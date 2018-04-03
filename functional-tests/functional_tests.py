eefbb4a84f789b0d2ee28a009d02105247d014c8
Jonathan Juares Beber <jonathanbeber@geru.com.br>
2018-04-02 21:23:18 -0300

    update the tests

--- functional-tests/functional_tests.py
+++ functional-tests/functional_tests.py
@@ -5,8 +5,28 @@ from selenium import webdriver
 host_address = os.environ.get("HOST_ADDRESS", 'localhost')
 
 options = webdriver.ChromeOptions()
+
 options.add_argument('--no-sandbox')
 browser = webdriver.Chrome('./chromedriver', options=options)
+# Enter the site
 browser.get('http://{}:8000'.format(host_address))
 
-assert 'Django' in browser.title
+# check the site's title
+assert 'To-Do' in browser.title
+
+# Insert text 'buy milk' in the new item box
+
+# confirm the action
+
+# Check if the item is in the page
+
+# Insert new item 'Clean the car'
+
+# check if the new item is in the page
+
+# Go to personal URL
+
+# check if 'buy milk' and 'Clean the car' are in a to-do list
+
+# exit
+browser.exit()
