78421bf67802aea23739de97b2aa8a6cb359b917
Jonatha Juares Beber <jonathanbeber@gmail.com>
2018-04-05 22:06:27 -0400

    checking to include items

--- functional-tests/functional_tests.py
+++ functional-tests/functional_tests.py
@@ -4,7 +4,7 @@ import time
 import unittest
 
 from selenium import webdriver
-
+from selenium.webdriver.common.keys import Keys
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
@@ -25,15 +25,32 @@ class NewVisitorTest(unittest.TestCase):
         )
 
         # check the site's title
-        self.assertIn('To-Do', self.browser.title)
+        self.assertIn('To-Do List', self.browser.title)
 
-        self.fail('Finish it!')
+        # check the site header
+        header_text = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('To-Do List', header_text)
+
         # Insert text 'buy milk' in the new item box
+        input_box = self.browser.find_element_by_id("new-item-box")
+        self.assertEqual(
+            input_box.get_attribute('placeholder'),
+            'insert a new item'
+        )
+        input_box.send_keys('buy milk')
 
         # confirm the action
+        input_box.send_keys(Keys.ENTER)
+        time.sleep(1)
 
         # Check if the item is in the page
+        todo_list = self.browser.find_element_by_id('todo-list')
+        rows = todo_list.find_elements_by_tag_name('<tr>')
+        selg.assertTrue(
+            any(row == "1. buy milk" for row in rows)
+        )
 
+        self.fail('Finish it!')
         # Insert new item 'Clean the car'
 
         # check if the new item is in the page
