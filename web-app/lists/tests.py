4ddfe08e2b735a636f16b1f522d3aed69c29233e
Jonatha Juares Beber <jonathanbeber@gmail.com>
2018-04-05 21:31:48 -0400

    add tests to check html format

--- web-app/lists/tests.py
+++ web-app/lists/tests.py
@@ -1,5 +1,6 @@
 from django.urls import resolve
 from django.test import TestCase
+from django.http import HttpRequest
 
 from lists.views import home_page
 
@@ -8,3 +9,11 @@ class HomePageTest(TestCase):
     def test_root_is_routing_rigth(self):
         found = resolve('/')
         self.assertEqual(found.func, home_page)
+
+    def test_html_format(self):
+        request = HttpRequest()
+        response = home_page(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startwith('<html>'))
+        self.assertIn('<title>To-do List</title>', html)
+        self.assertTrue(html.endswith('</html>'))
