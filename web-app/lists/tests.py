122d43f3f2481f04e24e2653ec3c8e21863f90b6
Jonatha Juares Beber <jonathanbeber@gmail.com>
2018-04-05 22:33:37 -0400

    keeping the tests simple

--- web-app/lists/tests.py
+++ web-app/lists/tests.py
@@ -1,19 +1,10 @@
 from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
 
 from lists.views import home_page
 
 
 class HomePageTest(TestCase):
-    def test_root_is_routing_rigth(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-
     def test_html_format(self):
-        request = HttpRequest()
-        response = home_page(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do List</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
+        response = self.client.get('/')
+        self.assertTemplateUsed(response, 'home.html')
