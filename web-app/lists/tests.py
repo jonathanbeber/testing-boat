8eebb98d4411702689dafc4116c928f1aa6e3993
Jonatha Juares Beber <jonathanbeber@gmail.com>
2018-04-07 16:39:09 -0400

    test POST

--- web-app/lists/tests.py
+++ web-app/lists/tests.py
@@ -8,3 +8,8 @@ class HomePageTest(TestCase):
     def test_html_format(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_handle_with_POST_requests(self):
+        new_item_text = 'buy milk'
+        response = self.client.post('/', data={'new-item': new_item_text})
+        self.assertTemplateUsed(response, 'home.html')
