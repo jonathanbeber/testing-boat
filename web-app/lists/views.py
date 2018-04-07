ce057589a4eae35d8976193c468c8792eec261b4
Jonatha Juares Beber <jonathanbeber@gmail.com>
2018-04-07 16:39:41 -0400

    handle post requests

--- web-app/lists/views.py
+++ web-app/lists/views.py
@@ -2,4 +2,11 @@ from django.shortcuts import render
 
 
 def home_page(request):
-    return render(request, 'home.html')
+    new_item_text = ''
+    if request.method == 'POST':
+        new_item_text = request.POST['new-item']
+    return render(
+        request,
+        'home.html',
+        {'new_item': new_item_text}
+    )
