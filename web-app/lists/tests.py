87bfbe6487ec82f3c501ac22e6c8445a1e2915b3
Jonatha Juares Beber <jonathanbeber@gmail.com>
2018-04-07 17:24:07 -0400

    add tests of models

--- web-app/lists/tests.py
+++ web-app/lists/tests.py
@@ -3,6 +3,8 @@ from django.test import TestCase
 
 from lists.views import home_page
 
+from lists.models import Item
+
 
 class HomePageTest(TestCase):
     def test_html_format(self):
@@ -13,3 +15,23 @@ class HomePageTest(TestCase):
         new_item_text = 'buy milk'
         response = self.client.post('/', data={'new-item': new_item_text})
         self.assertTemplateUsed(response, 'home.html')
+
+
+class ItemTest(TestCase):
+    def test_item_save(self):
+        first_item = Item()
+        first_item.text = 'Do some really important stuff'
+        first_item.save()
+
+        second_item = Item()
+        second_item.text = 'Do another really important stuff'
+        second_item.save()
+
+        items = Item.objects.all()
+        self.assertEqual(items.count(), 2)
+
+        first_saved_item = items[0]
+        second_saved_item = items[1]
+
+        self.assertEqual(first_item.text, first_saved_item.text)
+        self.assertEqual(second_item.text, second_saved_item.text)
