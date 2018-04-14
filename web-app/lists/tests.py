cc09e37e5888d343c25c1940adb83e9330a3ee5b
Jonatha Juares Beber <jonathanbeber@gmail.com>
2018-04-14 10:36:35 -0400

    add tests to list model

--- web-app/lists/tests.py
+++ web-app/lists/tests.py
@@ -3,7 +3,7 @@ from django.test import TestCase
 
 from lists.views import home_page
 
-from lists.models import Item
+from lists.models import Item, List
 
 
 class HomePageTest(TestCase):
@@ -46,15 +46,22 @@ class AddItemTest(TestCase):
         self.assertRedirects(response, '/list/the-only-existing-list')
 
 
-class ItemTest(TestCase):
+class ItemListTest(TestCase):
     def test_item_save(self):
+        first_list = List()
+        first_list.save()
+
         first_item = Item()
+        first_item.list = first_list
         first_item.text = 'Do some really important stuff'
         first_item.save()
 
         second_item = Item()
+        second_item.list = first_list
         second_item.text = 'Do another really important stuff'
         second_item.save()
+
+        self.assertEqual(first_list, List.objects.first())
 
         items = Item.objects.all()
         self.assertEqual(items.count(), 2)
@@ -62,5 +69,7 @@ class ItemTest(TestCase):
         first_saved_item = items[0]
         second_saved_item = items[1]
 
+        self.assertEqual(first_list, first_saved_item.list)
         self.assertEqual(first_item.text, first_saved_item.text)
+        self.assertEqual(first_list, second_saved_item.list)
         self.assertEqual(second_item.text, second_saved_item.text)
