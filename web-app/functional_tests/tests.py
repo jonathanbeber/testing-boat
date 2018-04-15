c7dd97b0fe0eb2f76815269b582e1c637766dfec
Jonatha Juares Beber <jonathanbeber@gmail.com>
2018-04-15 14:43:41 -0400

    testing CSS

--- web-app/functional_tests/tests.py
+++ web-app/functional_tests/tests.py
@@ -120,3 +120,21 @@ class NewVisitorTest(LiveServerTestCase):
             '1. ' + user_a_item_text,
             [row.text for row in self._get_todo_rows()]
         )
+
+
+    def test_layout_ans_styling(self):
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+        input_box = self._get_input_box()
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
+        self._set_new_item('new item')
+        input_box = self._get_input_box()
+        self.assertAlmostEqual(
+            input_box.location['x'] + input_box.size['width'] / 2,
+            512,
+            delta=10
+        )
