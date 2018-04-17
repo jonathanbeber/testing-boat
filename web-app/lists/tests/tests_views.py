74b27d1e076e23bfc78d258f1e667d5dc8520148
Jonatha Juares Beber <jonathanbeber@gmail.com>
2018-04-16 20:50:33 -0400

    split unit tests

--- web-app/lists/tests/tests_views.py
+++ web-app/lists/tests/tests_views.py
@@ -2,7 +2,6 @@ from django.urls import resolve
 from django.test import TestCase
 
 from lists.views import home_page
-
 from lists.models import Item, List
 
 
@@ -63,32 +62,3 @@ class AddItemTest(TestCase):
         self.assertRedirects(response, f'/list/{list_.id}/')
         response = self.client.post(f'/list/{list_.id}/new_item', data={'new-item': 'buy milk 2'})
         self.assertRedirects(response, f'/list/{list_.id}/')
-
-
-class ItemListTest(TestCase):
-    def test_item_save(self):
-        first_list = List()
-        first_list.save()
-
-        first_item = Item()
-        first_item.list = first_list
-        first_item.text = 'Do some really important stuff'
-        first_item.save()
-
-        second_item = Item()
-        second_item.list = first_list
-        second_item.text = 'Do another really important stuff'
-        second_item.save()
-
-        self.assertEqual(first_list, List.objects.first())
-
-        items = Item.objects.all()
-        self.assertEqual(items.count(), 2)
-
-        first_saved_item = items[0]
-        second_saved_item = items[1]
-
-        self.assertEqual(first_list, first_saved_item.list)
-        self.assertEqual(first_item.text, first_saved_item.text)
-        self.assertEqual(first_list, second_saved_item.list)
-        self.assertEqual(second_item.text, second_saved_item.text)
